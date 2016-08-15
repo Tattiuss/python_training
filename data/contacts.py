84decd999852430f27d063599bf3a3723f26ad1a
Tattiuss <tattiuss@gmail.com>
2016-08-15 21:52:16 +0300

    Генераторы тестовых данных для групп и контактов

---
+++ data/contacts.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from model.contact import Contact
+
+
+testdata = [
+    Contact(firstname="a", lastname="b", address="c", homephone="1", mobilephone="2", email="abc", email2="xyz"),
+    Contact(firstname="A", lastname="B", address="C", homephone="11", mobilephone="22", email="ABC", email2="XYZ")
+]
+
+
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,24 +1,9 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
-import pytest
-import random
-import string
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
-testdata = [Contact(firstname="", lastname="", address="", homephone="", mobilephone="",
-    email="", email2="")] + [
-    Contact(firstname=random_string("firstname", 10), lastname=random_string("lastname", 10),
-    address=random_string("address", 20), homephone=random_string("homephone", 15),
-    mobilephone=random_string("mobilephone", 15), email=random_string("email", 20), email2=random_string("email2", 20))
-    for i in range(7)
-]
-
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-
-def test_add_contact(app, contact):
+def test_add_contact(app, data_contacts):
+    contact = data_contacts
     old_contacts = app.contact.get_contact_list()
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
